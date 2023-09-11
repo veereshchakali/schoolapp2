7259a5cde6a78faebaa3c55f662f926895f9acb1
veereshchakali <veeresh4600@gmail.com>
2023-09-11 10:10:33 +0530

    approuting created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { AllStudentComponent } from './all-student/all-student.component';
+import { authenticationguardGuard } from './authenticationguard.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component:LoginComponent},
+  {path:'dashboard', component:DashboardComponent, canActivate :[authenticationguardGuard],children:[
+  {path:'home', component:HomeComponent},
+  {path:'create-student', component:CreateStudentComponent},
+  {path:'all-student', component:AllStudentComponent},
+  
+  ]},
+  {path:"", component:LoginComponent},
+  {path:'**', component:PagenotfoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { CreateStudentComponent } from './create-student/create-student.component';
+import { AllStudentComponent } from './all-student/all-student.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    PagenotfoundComponent,
+    HomeComponent,
+    CreateStudentComponent,
+    AllStudentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
