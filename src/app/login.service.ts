d155399f23992eacc1bb4ae1fa4f3f5759f62d13
veereshchakali <veeresh4600@gmail.com>
2023-09-11 19:39:17 +0530

    login page is correct mail to give open and wrong mail not open

--- src/app/login.service.ts
+++ src/app/login.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private _httpClient:HttpClient) { }
+
+  doLogin(data:any):Observable<any>{
+  return this._httpClient.post("https://reqres.in/api/login",data);
+  }
+
 }
+
+
+
+
+
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { LoginService } from '../login.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+
+public loginForm:FormGroup = new FormGroup({
+  email: new FormControl(),
+  password: new FormControl()
+});
+
+constructor(private loginservice:LoginService, private router:Router){}
+
+login(){
+  console.log(this.loginForm);
+  this.loginservice.doLogin(this.loginForm.value).subscribe(
+    (data:any)=>{
+      alert("Login Success");
+      this.router.navigateByUrl("/dashboard");
+      localStorage.setItem('token',data.token);
+    },
+    (err:any)=>{
+      alert("Login Failed");
+    }
+  )
+}
 
 }
